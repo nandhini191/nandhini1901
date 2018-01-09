fdb65e22ec16e50f17e02e981b6ac90aaea1de3e
nandhini191 <30830172+nandhini191@users.noreply.github.com>
2018-01-09 16:46:09 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num,pos,neg;
+printf("enter the number")
+scanf("%d"&num);
+if(num==positive)
+{
+printf("number is positive");
+}
+else
+{
+printf("number is negative);
+}
+}
